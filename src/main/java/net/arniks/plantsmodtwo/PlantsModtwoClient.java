d43c0e31c10d26ac9bf3d7ad2f56893d409d43fe
ArNiks_ <arseniy2312005@gmail.com>
2024-08-13 20:47:29 +0800

    1 add
    ff

---
+++ src/main/java/net/arniks/plantsmodtwo/PlantsModtwoClient.java
@@ -0,0 +1,10 @@
+package net.arniks.plantsmodtwo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PlantsModtwoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
